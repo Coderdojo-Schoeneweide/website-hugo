f99bc43e7056a6d203c3305372d2e0d136888483
Luis Hankel <kaes3kuch3n@gmail.com>
2024-04-16 20:05:28 +0200

    Some small structural changes to improve readability

--- content/dojo-projects/finde_mich.md
+++ content/dojo-projects/finde_mich.md
@@ -1,6 +1,6 @@
 ---
 title: "Finde mich von Ottikar"
-date: 2023-26-07T19:08:24+02:00
+date: 2023-07-26T19:08:24+02:00
 projectLink: "https://scratch.mit.edu/projects/846766369"
 ---
 
--- themes/coderdojoschoeneweide/static/js/filters.js
+++ themes/coderdojoschoeneweide/static/js/filters.js
@@ -1,34 +1,65 @@
-let showFilter = false;
-let filterArrow;
-let filter;
 let results;
 let section_blogs;
 
-let ageFilter = [];
-let durationFilter = [];
-let readingFilter = [];
-let titleFilter = [];
-let tagFilters = [];
+/**
+ * Definitions for all possible filters
+ * @type {Record<string, {value: any, selector: string, type: string, check: (value: any) => boolean}>}
+ */
+const filters = {
+    age: {
+        value: "",
+        selector: "#age-filter",
+        type: "select",
+        check: (value) => filters.age.value.length === 0 || value.toLowerCase() === filters.age.value
+    },
+    duration: {
+        value: "",
+        selector: "#duration-filter",
+        type: "select",
+        check: (value) => filters.duration.value.length === 0 || value.toLowerCase() === filters.duration.value
+    },
+    readingTime: {
+        value: "",
+        selector: "#reading-time-filter",
+        type: "select",
+        check: (value) => filters.readingTime.value.length === 0 || value.toLowerCase() === filters.readingTime.value
+    },
+    title: {
+        value: "",
+        selector: "#title-search-filter",
+        type: "input",
+        check: (value) => filters.title.value.length === 0 || value.toLowerCase().includes(filters.title.value)
+    },
+    tag: {
+        value: [],
+        selector: "#tag-filters input",
+        type: "checkbox",
+        check: (value) => filters.tag.value.length === 0 || filters.tag.value.every(x => value.includes(x))
+    }
+};
 
 document.addEventListener("DOMContentLoaded", () => {
+    const filterSection = document.querySelector("#filter");
+    // Abort this script if there are no filters on this page
+    if (!filterSection) return;
+
+
     section_blogs = document.querySelector("#all-posts");
-    filter = document.querySelector("#filter");
     results = document.querySelectorAll("#results #result-item");
 
     const filterToggle = document.querySelector("#filter-toggle");
-    filterArrow = filterToggle.querySelector("span");
+    const filterArrow = filterToggle.querySelector("span");
 
+    filterToggle.addEventListener("click", toggleFilter(filterSection, filterArrow));
 
     try {
-        filterToggle.addEventListener("click", toggleFilter);
-
         //for branch redesign!
         document.querySelectorAll("#btn-tag-filters button").forEach(button => button.addEventListener("click", evt => {
             //check if tag already in tagFilters
             if (tagFilters.findIndex(item => item === evt.target.value) === -1) {
                 tagFilters.push(evt.target.value);
                 //check the tag in the filter-checkbox
-                filter.querySelectorAll("#tag-filters input").forEach(checkbox => {
+                filterSection.querySelectorAll("#tag-filters input").forEach(checkbox => {
                     if (checkbox.value === evt.target.value) {
                         checkbox.checked = true;
                     }
@@ -40,86 +71,76 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
-    const filters = new Map();
-    //Mapping -> Filter , [selector (html element id), html element]
-    //Filter blogs + workshop
-    filters.set(tagFilters, ["#tag-filters input", "checkbox"]);
-    //Filter blogs
-    filters.set(titleFilter, ["#title-search-filter", "input"]);
-    filters.set(readingFilter, ["#readingTime-filters", "select"]);
-    //Filter workshop
-    filters.set(ageFilter, ["#age-filter", "select"]);
-    filters.set(durationFilter, ["#duration-filter", "select"]);
-
-    addEventListenerToFilter(filters);
+    for (const filter in filters) {
+        addEventListenerToFilter(filterSection, filters[filter]);
+    }
 });
 
-function toggleFilter(event) {
-    event.preventDefault();
-    showFilter = !showFilter;
-    if (showFilter) {
-        filterArrow.innerText = "▾";
-        filter.classList.add("show");
-    } else {
-        filterArrow.innerText = "▸";
-        filter.classList.remove("show");
+function toggleFilter(filterSection, filterArrow) {
+    let showFilter = false;
+
+    return (event) => {
+        event.preventDefault();
+        showFilter = !showFilter;
+        if (showFilter) {
+            filterArrow.innerText = "▾";
+            filterSection.classList.add("show");
+        } else {
+            filterArrow.innerText = "▸";
+            filterSection.classList.remove("show");
+        }
     }
 }
 
-function addEventListenerToFilter(filters) {
-    filters.forEach((listElement, filterName) => {
-        try {
-            const selector = listElement[0];
-            const type = listElement[1];
+/**
+ * Add change event listener to a filter element on the current list page
+ * @param {HTMLElement} filterSection The HTML element containing all the filters
+ * @param {{value: any[], selector: string, type: string}} filter The filter object
+ */
+function addEventListenerToFilter(filterSection, filter) {
+    switch (filter.type) {
+        case "checkbox":
+            filterSection.querySelectorAll(filter.selector)
+                .forEach(checkbox => checkbox.addEventListener("change", (e) => checkboxFilterChange(e, filter)));
+            break;
+        case "input":
+            filterSection.querySelector(filter.selector)?.addEventListener("input", (e) => filterChange(e, filter));
+            break;
+        default:
+            filterSection.querySelector(filter.selector)?.addEventListener("change", (e) => filterChange(e, filter));
+            break;
+    }
+}
 
-            if (type === "checkbox") {
-                filter.querySelectorAll(selector)
-                    .forEach(checkbox => checkbox.addEventListener("change", event => {
-                        if (event.target.checked)
-                            filterName.push(event.target.value);
-                        else
-                            filterName.splice(filterName.indexOf(event.target.value), 1);
-                        updateFilterResults();
-                    }));
-            } else if (type === "input") {
-                filter.querySelector(selector).addEventListener("input", event => {
-                    const value = event.target.value !== "" ? event.target.value : null;
-                    filterName.pop();
-                    if (value != null) {
-                        filterName.push(value.toLowerCase());
-                    }
-                    updateFilterResults();
-                });
-            } else {
-                filter.querySelector(selector).addEventListener("change", event => {
-                    const value = event.target.value !== "" ? event.target.value : null;
-                    filterName.pop();
-                    if (value != null) {
-                        filterName.push(value.toLowerCase());
-                    }
-                    updateFilterResults();
-                });
-            }
-        } catch (e) {
-            // adding an eventListener can cause an error if selector does not exist
-            // Some of the filters don't exist on the other pages
-            // e.g. workshop page does not have a selector called #title-search-filter
-        }
-    })
+function checkboxFilterChange(event, filter) {
+    if (event.target.checked)
+        filter.value.push(event.target.value);
+    else
+        filter.value.splice(filter.value.indexOf(event.target.value), 1);
+    updateFilterResults();
+}
+
+function filterChange(event, filter) {
+    filter.value = event.target.value.toLowerCase();
+    updateFilterResults();
 }
 
 function updateFilterResults() {
     if (section_blogs != null) {
         results.forEach(blog => {
-            blog.hidden = !((readingFilter.length === 0 || blog.dataset.readingtime === readingFilter[0]) &&
-                (titleFilter.length === 0 || blog.dataset.title.toLowerCase().includes(titleFilter[0]) === true) &&
-                (tagFilters.length === 0 || tagFilters.every(c => blog.dataset.tags.indexOf(c) >= 0)));
+            blog.hidden = !(
+                filters.readingTime.check(blog.dataset.readingtime) &&
+                filters.title.check(blog.dataset.title) &&
+                filters.tag.check(blog.dataset.tags)
+            );
         });
     } else {
         results.forEach(workshop => {
-            workshop.hidden = !((ageFilter.length === 0 || workshop.dataset.age.toLowerCase() === ageFilter[0]) &&
-                (durationFilter.length === 0 || workshop.dataset.duration.toLowerCase() === durationFilter[0]) &&
-                (tagFilters.length === 0 || tagFilters.every(c => workshop.dataset.tags.indexOf(c) >= 0)));
+            workshop.hidden = !(
+                filters.age.check(workshop.dataset.age.toLowerCase()) &&
+                filters.duration.check(workshop.dataset.duration) &&
+                filters.tag.check(workshop.dataset.tags)
+            );
         });
     }
 }
